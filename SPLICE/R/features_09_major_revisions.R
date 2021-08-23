8db5aefb44920cc18fd783591be076fd85e0384d
Melantha Wang <wang.melantha@gmail.com>
2021-08-23 23:39:43 +1000

    Update implementation of maRev suite functions (rfun/paramfun)

--- SPLICE/R/features_09_major_revisions.R
+++ SPLICE/R/features_09_major_revisions.R
@@ -62,41 +62,71 @@ claim_maRev_no <- function(
   claims,
   rfun,
   paramfun,
-  # claim_size_benchmark,
-  # maRev_no_function,
   frequency_vector = claims$frequency_vector,
   claim_size_list = claims$claim_size_list,
   ...
 ) {
 
-
-
+  if (!missing(rfun) & missing(paramfun)) {
+    # we will see if we can continue without parameterisation
+    # e.g. if the input rfun is directly a function of claim_size, and no
+    # transformation is required
+    paramfun <- function(...) {
+      c(...)
+    }
+    # paramfun_filled indicates whether an "empty" paramfun is taken by default
+    paramfun_filled <- TRUE
+  } else {
+    paramfun_filled <- FALSE
+  }
 
   # default function to simulate the number of major revisions
-  # NOTE: the maRev_size_function takes as an assumption that there are max 3
+  # NOTE: the default function takes as an assumption that there are max 3
   # major revisions -> if user wants to change this, they need to take care of
   # the later module (maRev_size) too
-  if (missing(maRev_no_function)) {
-    maRev_no_function <- function(claim_size) {
+  if (missing(rfun)) {
+    rfun <- function(
+      # n = number of observations/claims
+      n, claim_size, claim_size_benchmark) {
+
+      if (n != length(claim_size)) {
+        stop("The number of claims does not match.")
+      }
+
       # inherit ref_claim from SynthETIC
       ref_claim <- SynthETIC::return_parameters()[1]
-      claim_size_benchmark <- 0.075 * ref_claim
-      if (claim_size <= claim_size_benchmark) {
-        k <- 1
-      } else {
-        Pr2 <- 0.1 + 0.3 *
-          min(1, (claim_size - 0.075*ref_claim)/(0.925*ref_claim))
-        Pr3 <- 0.5 *
-          min(1, max(0, claim_size - 0.25*ref_claim)/(0.75*ref_claim))
-        Pr1 <- 1 - Pr2 - Pr3
-        k <- sample(c(1, 2, 3), size = 1, replace = TRUE, prob = c(Pr1, Pr2, Pr3))
+
+      # default benchmark value
+      if (missing(claim_size_benchmark)) {
+        claim_size_benchmark <- 0.075 * ref_claim
+      }
+
+      k <- vector(length = n)
+      for (i in 1:n) {
+        if (claim_size[i] <= claim_size_benchmark) {
+          k[i] <- 1
+        } else {
+          Pr2 <- 0.1 + 0.3 *
+            min(1, (claim_size[i] - 0.075*ref_claim)/(0.925*ref_claim))
+          Pr3 <- 0.5 *
+            min(1, max(0, claim_size[i] - 0.25*ref_claim)/(0.75*ref_claim))
+          Pr1 <- 1 - Pr2 - Pr3
+          k[i] <- sample(c(1, 2, 3), size = 1, replace = TRUE, prob = c(Pr1, Pr2, Pr3))
+        }
       }
 
       k
     }
+
+    # the default rfun directly takes claim_size as an input, so the "empty"
+    # paramfun would do the trick
+    paramfun <- function(...) {
+      c(...)
+    }
   }
 
   I <- length(frequency_vector)
+  no_claims <- sum(frequency_vector)
   maRev <- vector("list", I)
   # maRev_unit stores all major revision information on a single claim
   maRev_unit <- list(
@@ -105,11 +135,40 @@ claim_maRev_no <- function(
     maRev_multiplier = NA,
     maRev_atP = NA
   )
+  # set up the simulation parameters
+  params <- mapply(paramfun,
+                   claim_size = unlist(claim_size_list, use.names = FALSE),
+                   ...)
+
+  # if params only has one parameter, asplit() won't work
+  if (!is.null(names(params))) {
+    params_split <- split(unname(params), names(params))
+  } else if (length(params)) {
+    params_split <- asplit(params, 1)
+  }
+
+  # do.call rfun, but ignore unused arguments
+  args <- as.list(params_split)
+  keep_names <- c(intersect(names(args), names(formals(rfun))))
+  keep_formals <- c(list(n = no_claims), args[keep_names])
+
+  if (paramfun_filled) {
+    # check if the "empty" paramfun is sufficient to call the rfun
+    tt <- try(no_maRev_vect <- do.call(rfun, keep_formals), TRUE)
+    if (methods::is(tt, "try-error")) {
+      stop("need to specify 'paramfun' for the sampling distribution")
+    }
+  } else {
+    no_maRev_vect <- do.call(rfun, keep_formals)
+  }
+
+  curr <- 1 # curr tracks the claim number
   for (i in 1:I) {
     maRev[[i]] <- vector("list", frequency_vector[i])
     for (j in 1:frequency_vector[i]) {
       maRev[[i]][[j]] <- maRev_unit
-      maRev[[i]][[j]]$maRev_no <- maRev_no_function(claim_size_list[[i]][j])
+      maRev[[i]][[j]]$maRev_no <- no_maRev_vect[curr]
+      curr <- curr + 1
     }
   }
 
@@ -165,24 +224,39 @@ claim_maRev_no <- function(
 claim_maRev_time <- function(
   claims,
   maRev_list,
-  maRev_time_function,
+  rfun,
+  paramfun,
   claim_size_list = claims$claim_size_list,
   settlement_list = claims$settlement_list,
-  payment_delay_list = claims$payment_delay_list
+  payment_delay_list = claims$payment_delay_list,
+  ...
 ) {
 
+  if (!missing(rfun) & missing(paramfun)) {
+    # we will see if we can continue without parameterisation
+    # e.g. if the input rfun is directly a function of claim_size, and no
+    # transformation is required
+    paramfun <- function(...) {
+      c(...)
+    }
+    # paramfun_filled indicates whether an "empty" paramfun is taken by default
+    paramfun_filled <- TRUE
+  } else {
+    paramfun_filled <- FALSE
+  }
+
   # default function to simulate the timing of major revisions (from notification)
-  if (missing(maRev_time_function)) {
-    maRev_time_function <- function(maRev_no, claim_size, setldel, payment_delays) {
-      k <- maRev_no
-      no_pmt <- length(payment_delays)
-      maRev_time <- rep(NA, times = k)
+  if (missing(rfun)) {
+    rfun <- function(n, claim_size, setldel, penultimate_delay) {
+      # n = number of simulations, here n should be the number of major revisions
+      # penultimate_delay = delay from notification to penultimate payment
+      maRev_time <- rep(NA, times = n)
       maRev_time[1] <- 0 # first revision at notification
 
       # inherit ref_claim from SynthETIC
       ref_claim <- SynthETIC::return_parameters()[2]
 
-      if (k > 1) {
+      if (n > 1) {
         # if the claim has multiple major revisions
         # does the last revision occur exactly at the second last partial payment?
         p <- 0.2 *
@@ -190,38 +264,78 @@ claim_maRev_time <- function(
         at_second_last_pmt <- sample(c(0, 1), size = 1, replace = TRUE, prob = c(1-p, p))
         if (at_second_last_pmt == 0) {
           # no revision at second last payment
-          maRev_time[2:k] <- sort(rtri(k - 1, min = setldel/3, max = setldel, mode = setldel/3))
+          maRev_time[2:n] <- sort(rtri(n - 1, min = setldel/3, max = setldel, mode = setldel/3))
         } else {
           # revision at second last payment
-          maRev_time[k] <- sum(payment_delays[1:(no_pmt - 1)])
-          if (k > 2) {
-            maRev_time[2:(k-1)] <- sort(
-              rtri(k - 2, min = maRev_time[k]/3, max = maRev_time[k], mode = maRev_time[k]/3))
+          maRev_time[n] <- penultimate_delay
+          if (n > 2) {
+            maRev_time[2:(n-1)] <- sort(
+              rtri(n - 2, min = maRev_time[n]/3, max = maRev_time[n], mode = maRev_time[n]/3))
           }
         }
       }
       maRev_time
     }
+
+    # the paramfun needs to account for the "computation" of penultimate_delay
+    # from the payment_delay_list
+    paramfun <- function(payment_delays, ...) {
+      c(penultimate_delay = sum(payment_delays[1:length(payment_delays)-1]),
+        ...)
+    }
   }
 
   I <- length(maRev_list)
+  params <- mapply(
+    paramfun,
+    claim_size = unlist(claim_size_list, use.names = FALSE),
+    setldel = unlist(settlement_list, use.names = FALSE),
+    # reduce payment_delays by one level only (to claim level)
+    payment_delays = unlist(payment_delay_list, use.names = F, recursive = F),
+    ...)
+
+  # if params only has one parameter, asplit() won't work
+  if (!is.null(names(params))) {
+    params_split <- split(unname(params), names(params))
+  } else if (length(params)) {
+    params_split <- asplit(params, 1)
+  }
+
+  # do.call rfun, but ignore unused arguments
+  args <- as.list(params_split)
+  keep_names <- c(intersect(names(args), names(formals(rfun))))
+  keep_formals <- c(args[keep_names])
+
+  # turn keep_formals, which is a list of arguments, to a dataframe
+  args_df <- do.call(rbind, keep_formals)
+  # in the dataframe, each row represents a parameter, and each column gives the
+  # parameter values for a specific claim
+
+  curr <- 1
   for (i in 1:I) {
     for (j in 1 : length(maRev_list[[i]])) {
 
       k <- maRev_list[[i]][[j]]$maRev_no
-      payment_delays <- payment_delay_list[[i]][[j]]
 
-      maRev_time <- maRev_time_function(
-        maRev_no = k,
-        claim_size = claim_size_list[[i]][j],
-        setldel = settlement_list[[i]][j],
-        payment_delays = payment_delays)
+      if (paramfun_filled) {
+        tt <- try(maRev_list[[i]][[j]]$maRev_time <- do.call(
+          rfun, c(as.list(args_df[, curr]), n = k)))
+        if (methods::is(tt, "try-error")) {
+          stop("need to specify 'paramfun' for the sampling distribution")
+        }
+      } else {
+        maRev_list[[i]][[j]]$maRev_time <- do.call(
+          rfun, c(as.list(args_df[, curr]), n = k))
+      }
 
-      maRev_list[[i]][[j]]$maRev_time <- maRev_time
       # is there a revision at second last payment?
+      payment_delays <- payment_delay_list[[i]][[j]]
       no_pmt <- length(payment_delays)
       maRev_list[[i]][[j]]$maRev_atP <- ifelse(
-        maRev_time[k] == sum(payment_delays[1:(no_pmt - 1)]), 1, 0)
+        maRev_list[[i]][[j]]$maRev_time[k] == sum(payment_delays[1:(no_pmt - 1)]),
+        1, 0)
+
+      curr <- curr + 1
     }
   }
 
@@ -277,19 +391,35 @@ claim_maRev_time <- function(
 #' @export
 claim_maRev_size <- function(
   maRev_list,
-  maRev_size_function
+  rfun,
+  paramfun,
+  ...
 ) {
+
+  if (!missing(rfun) & missing(paramfun)) {
+    # we will see if we can continue without parameterisation
+    # e.g. if the input rfun is directly a function of claim_size, and no
+    # transformation is required
+    paramfun <- function(...) {
+      c(...)
+    }
+    # paramfun_filled indicates whether an "empty" paramfun is taken by default
+    paramfun_filled <- TRUE
+  } else {
+    paramfun_filled <- FALSE
+  }
 
   # default function to simulate multiplier sizes
   # NOTE: this only works for up to 3 major revisions -> if user has adjusted
   # the maRev_no_function, they will need to adjust this accordingly
-  if (missing(maRev_size_function)) {
-    maRev_size_function <- function(maRev_no) {
-      maRev_multiplier <- rep(NA, times = maRev_no)
+  if (missing(rfun)) {
+    rfun <- function(n) {
+      # n = number of simulations, here n should be the number of major revisions
+      maRev_multiplier <- rep(NA, times = n)
       maRev_multiplier[1] <- 1 # for first revision (at notification)
-      if (maRev_no > 1) { # if the claim has multiple major revisions
+      if (n > 1) { # if the claim has multiple major revisions
         maRev_multiplier[2] <- stats::rlnorm(n = 1, meanlog = 1.8, sdlog = 0.2)
-        if (maRev_no > 2) {
+        if (n > 2) {
           mu <- 1 + 0.07 * (6 - maRev_multiplier[2])
           maRev_multiplier[3] <- stats::rlnorm(n = 1, meanlog = mu, sdlog = 0.1)
         }
@@ -297,13 +427,53 @@ claim_maRev_size <- function(
 
       maRev_multiplier
     }
+
+    # the default rfun does not depend on other claim characteristics, so the
+    # "empty" paramfun would do the trick
+    paramfun <- function(...) {
+      c(...)
+    }
+
   }
 
   I <- length(maRev_list)
+  params <- mapply(paramfun, ...)
+
+  # if params only has one parameter, asplit() won't work
+  if (!is.null(names(params))) {
+    params_split <- split(unname(params), names(params))
+  } else if (length(params)) {
+    params_split <- asplit(params, 1)
+  }
+
+  # do.call rfun, but ignore unused arguments
+  args <- as.list(params_split)
+  keep_names <- c(intersect(names(args), names(formals(rfun))))
+  keep_formals <- c(args[keep_names])
+
+  # turn keep_formals, which is a list of arguments, to a dataframe
+  args_df <- do.call(rbind, keep_formals)
+  # in the dataframe, each row represents a parameter, and each column gives the
+  # parameter values for a specific claim
+
+  curr <- 1
   for (i in 1:I) {
     for (j in 1 : length(maRev_list[[i]])) {
+
       k <- maRev_list[[i]][[j]]$maRev_no
-      maRev_list[[i]][[j]]$maRev_multiplier <- maRev_size_function(maRev_no = k)
+
+      if (paramfun_filled) {
+        tt <- try(maRev_list[[i]][[j]]$maRev_multiplier <- do.call(
+          rfun, c(as.list(args_df[, curr]), n = k)))
+        if (methods::is(tt, "try-error")) {
+          stop("need to specify 'paramfun' for the sampling distribution")
+        }
+      } else {
+        maRev_list[[i]][[j]]$maRev_multiplier <- do.call(
+          rfun, c(as.list(args_df[, curr]), n = k))
+      }
+
+      curr <- curr + 1
     }
   }
 
