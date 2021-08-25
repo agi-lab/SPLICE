f3cfeff2ac68a008c9e3ec3f09e3053dd64aa1b4
Melantha Wang <wang.melantha@gmail.com>
2021-08-26 02:15:06 +1000

    Update test data name and labels

--- SPLICE/R/data.R
+++ SPLICE/R/data.R
@@ -5,15 +5,18 @@
 #' Incurred Case Estimates Dataset
 #'
 #' A dataset of 31,250 records of transactions (partial payments and incurred
-#' revisions) associated with the 3,624 claims in `test_claim_dataset`. The
-#' `_inflated` version includes inflation adjustment in the case estimates.
+#' revisions) associated with the 3,624 claims described in `SynthETIC`'s
+#' \code{\link[SynthETIC]{test_claim_dataset}}. The `_inflated` version includes
+#' inflation adjustment in the case estimates, while the `_noInf` version
+#' excludes any inflation effects.
 #'
 #' @format A data frame with 31,250 rows and 9 variables:
 #' \describe{
 #'   \item{claim_no}{claim number, which uniquely characterises each claim.}
 #'   \item{claim_size}{size of the claim (in constant dollar values).}
-#'   \item{txn_time}{delay from notification to the subject transaction.}
-#'   \item{txn_t}{double; time of transaction (on a continuous time scale).}
+#'   \item{txn_time}{double; the "absolute" time of transaction (on a continuous
+#'                   time scale).}
+#'   \item{txn_delay}{delay from notification to the subject transaction.}
 #'   \item{txn_type}{character; nature of the transactions, "Ma" for major
 #'                   revision, "Mi" for minor revision, "P" for payment,
 #'                   "PMa" for major revision coincident with a payment,
@@ -29,7 +32,7 @@
 #' }
 #' @seealso \code{\link{generate_incurred_dataset}}
 #' @name test_incurred_dataset
-"test_incurred_dataset"
+"test_incurred_dataset_noInf"
 
 #' @rdname test_incurred_dataset
 #' @format NULL
--- SPLICE/data-raw/test_data.R
+++ SPLICE/data-raw/test_data.R
@@ -4,22 +4,22 @@
 
 ## v2 incurred program
 set.seed(20201006)
-claims <- SynthETIC::test_claims_object
-major <- claim_maRev_no(claims)
-major <- claim_maRev_time(claims, major)
+test_claims <- SynthETIC::test_claims_object
+major <- claim_maRev_no(test_claims)
+major <- claim_maRev_time(test_claims, major)
 major <- claim_maRev_size(major)
 
 # minor revisions
-minor <- claim_miRev_no(claims)
-minor <- claim_miRev_time(claims, minor)
-minor <- claim_miRev_size(claims, major, minor)
+minor <- claim_miRev_no(test_claims)
+minor <- claim_miRev_time(test_claims, minor)
+minor <- claim_miRev_size(test_claims, major, minor)
 
-test <- claim_history(claims, major, minor)
-test_inflated <- claim_history(claims, major, minor, inflated = TRUE,
+test <- claim_history(test_claims, major, minor)
+test_inflated <- claim_history(test_claims, major, minor, inflated = TRUE,
                                base_inflation_vector = rep((1 + 0.02)^(1/4) - 1, times = 80))
-test_incurred_dataset <- generate_incurred_dataset(claims, test)
-test_incurred_dataset_inflated <- generate_incurred_dataset(claims, test_inflated)
+test_incurred_dataset_noInf <- generate_incurred_dataset(test_claims, test)
+test_incurred_dataset_inflated <- generate_incurred_dataset(test_claims, test_inflated)
 
-usethis::use_data(test_incurred_dataset, overwrite = TRUE)
+usethis::use_data(test_incurred_dataset_noInf, overwrite = TRUE)
 usethis::use_data(test_incurred_dataset_inflated, overwrite = TRUE)
 
