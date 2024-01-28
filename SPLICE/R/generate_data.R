b625bcaa5723fa5822510449d6a1c29f6dd6503c
William Ho <yunwaiho@gmail.com>
2024-01-29 09:39:21 +1100

    Updated generate_data covariate-adjusted datasets with seed set in covariates simulation

--- SPLICE/R/generate_data.R
+++ SPLICE/R/generate_data.R
@@ -319,7 +319,11 @@ generate_data <- function(
   claim_sizes <- claim_size(freq_vector)
   # M2a: Simulation of Covariates
   if (!is.null(covariates_obj) & isa(covariates_obj, "covariates")) {
-    claim_size_covariates <- claim_size_adj(covariates_obj, claim_sizes)
+    claim_size_covariates <- claim_size_adj(
+        covariates_obj,
+        claim_sizes,
+        random_seed = random_seed
+    )
     covariates_data_obj <- claim_size_covariates$covariates_data
     claim_sizes <- claim_size_covariates$claim_size_adj
     output$covariates_data <- covariates_data_obj
